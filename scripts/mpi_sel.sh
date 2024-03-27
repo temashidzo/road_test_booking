ce5558b51f59ab68a31947603bad5e6a4719df85
temashidzo <temashidzo@gmail.com>
2024-03-27 04:18:21 +0000

    scripts updated

--- scripts/mpi_sel.sh
+++ scripts/mpi_sel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /root/Docs/MPI_Bot/.venv/bin/activate
-nohup python3 -u /root/Docs/MPI_Bot/src/mpi_sel.py >> /root/Docs/MPI_Bot/logs/script.log &
+source /projects/mpi/.venv/bin/activate
+nohup python3 -u /projects/mpi/src/mpi.py >> /projects/mpi/logs/mpi.log &
--- scripts/telegram.sh
+++ scripts/telegram.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /root/Docs/MPI_Bot/.venv/bin/activate
-nohup python3 -u /root/Docs/MPI_Bot/src/telegram.py >> /root/Docs/MPI_Bot/logs/telegram.log &
+source /projects/mpi/.venv/bin/activate
+nohup python3 -u /projects/mpi/src/telegram.py >> /projects/mpi/logs/telegram.log &
